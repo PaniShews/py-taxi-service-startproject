e458a4cf7b20e22b6cfe5e8f37c0c4c83268e36b
PaniShews <kpetruh@gmail.com>
2026-05-13 21:36:34 +0300

    Solution

--- taxi/apps.py
+++ taxi/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class TaxiConfig(AppConfig):
     name = 'taxi'
+    default_auto_field = 'django.db.models.BigAutoField'
--- taxi/models.py
+++ taxi/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     country = models.CharField(max_length=100)
 
 
